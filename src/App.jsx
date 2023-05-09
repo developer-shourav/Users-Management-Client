67863f9af2371dcce9995a8e7c464f0f1e9c449e
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-10 00:47:12 +0600

    privous code removed

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,6 @@ import './App.css'
 const App = () => {
   const [users, setUsers] = useState([]);
 
-  const handleAddUser = event => {
-    event.preventDefault()
-    const form = event.target;
-    const name = form.name.value;
-    const email = form.email.value;
-    const user = {
-       name,
-       email
-    };
-    console.log(user);
-    fetch('http://localhost:8000/users', {
-       method:'POST',
-       headers : {
-        'content-type':'application/json'
-       },
-       body: JSON.stringify(user)
-    })
-    .then( res => res.json())
-    .then( data => {
-      console.log(data);
-    })
-    form.reset()
-  }
 
   useEffect(() => {
     fetch('http://localhost:8000/users')
@@ -38,13 +15,7 @@ const App = () => {
       <h2>Total Users:{users?.length}</h2>
 
 
-      <form onSubmit={handleAddUser}>
-        <input type="text" name="name" id="" />
-        <br />
-        <input type="email" name="email" id="" />
-        <br />
-        <input type="submit" value="Add User" />
-      </form>
+      
 
 
 
